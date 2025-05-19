c1e07c0d3c7d585d7f787998fbf58fa5f848596f
Matheus Moreira <matheustjp111@gmail.com>
2025-05-19 10:34:43 -0300

    chore: setup typescript, fastify and eslint

---
+++ server/eslint.config.mjs
@@ -0,0 +1,32 @@
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+import js from '@eslint/js'
+import { FlatCompat } from '@eslint/eslintrc'
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+})
+
+export default [
+  {
+    ignores: ['**/node_modules', '**/build'],
+  },
+  ...compat.extends('@rocketseat/eslint-config/node'),
+  {
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+    },
+
+    rules: {
+      'simple-import-sort/imports': 'error',
+      camelcase: 'off',
+      'no-useless-catch': 'off',
+      'no-useless-constructor': 'off',
+    },
+  },
+]

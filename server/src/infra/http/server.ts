18d3cf1dbbb0bb1d0ed921c2b03645f7786677b2
Matheus Moreira <matheustjp111@gmail.com>
2025-05-19 11:15:20 -0300

    chore: setup validation, serializer and error handler on fastify

--- server/src/infra/http/server.ts
+++ server/src/infra/http/server.ts
@@ -1,9 +1,35 @@
 import fastifyCors from '@fastify/cors'
 import fastify from 'fastify'
+import {
+  hasZodFastifySchemaValidationErrors,
+  serializerCompiler,
+  validatorCompiler,
+} from 'fastify-type-provider-zod'
+
+import { env } from '@/env'
 
 const server = fastify()
 
+server.setValidatorCompiler(validatorCompiler)
+server.setSerializerCompiler(serializerCompiler)
+
 server.register(fastifyCors, { origin: '*' })
+
+server.setErrorHandler((error, _, reply) => {
+  if (hasZodFastifySchemaValidationErrors(error)) {
+    return reply
+      .status(400)
+      .send({ message: 'Validation error.', issues: error.validation })
+  }
+
+  if (env.NODE_ENV !== 'production') {
+    console.error(error)
+  } else {
+    // TODO: Here we should log to a external tool like DataDog/NewRelic/Sentry
+  }
+
+  return reply.status(500).send({ message: 'Internal server error.' })
+})
 
 server.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
   console.log('🚀 HTTP server running!')

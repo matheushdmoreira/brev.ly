46ec9364bbf9c98753b7bc9018af3bf1784e4fc2
Matheus Moreira <matheustjp111@gmail.com>
2025-05-29 11:13:14 -0300

    refactor: border top in empty and loading components

--- web/src/components/empty-links.tsx
+++ web/src/components/empty-links.tsx
@@ -2,7 +2,7 @@ import { LinkIcon } from '@phosphor-icons/react'
 
 export function EmptyLinks() {
   return (
-    <div className="flex flex-col items-center gap-3 pt-4 pb-6 px-0 [&>svg]:text-gray-400">
+    <div className="flex flex-col items-center gap-3 pt-4 pb-6 px-0 [&>svg]:text-gray-400 border-gray-200 border-t">
       <LinkIcon size={32} />
 
       <p className="text-gray-500 text-xs font-normal leading-[14px] uppercase">
--- web/src/components/loading-links.tsx
+++ web/src/components/loading-links.tsx
@@ -2,7 +2,7 @@ import { SpinnerIcon } from '@phosphor-icons/react'
 
 export function LoadingLinks() {
   return (
-    <div className="flex flex-col items-center gap-3 pt-4 pb-6 px-0 [&>svg]:text-gray-400">
+    <div className="flex flex-col items-center gap-3 pt-4 pb-6 px-0 [&>svg]:text-gray-400 border-gray-200 border-t">
       <SpinnerIcon className="animate-spin" size={32} />
 
       <p className="text-gray-500 text-xs font-normal leading-[14px] uppercase">
